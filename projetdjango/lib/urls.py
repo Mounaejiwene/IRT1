ff3ec9949e9620e53c1c0e548fd6be78cadab7c9
Mounaejiwene <161493939+Mounaejiwene@users.noreply.github.com>
2024-05-15 18:35:03 +0200

    Update urls.py

--- projetdjango/lib/urls.py
+++ projetdjango/lib/urls.py
@@ -2,6 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('Ajouter un livre/', views.ajouter_livre, name='add_book'),
+    path('about/', views.about_page,name = 'about'),
+    path('', views.liste_livres,name = 'Liste des livres'),
+    path('book/<str:book_name>/',views.detail_livre, name='detail_livre'),
+    path('recherche/', views.recherche, name='recherche'),
     path('Signup/',views.Signup, name='Signup'),
     path('Signin/',views.Signin, name='Signin'),
     path('Signin/forgot_password/',views.forgotten_password,name='fpassword'),
