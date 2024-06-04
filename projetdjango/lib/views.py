d13aca24e965affa937e4346f02f4ebc03ebd84b
Ahmedou <23083@gmail.com>
2024-06-04 07:40:29 -0700

    ajouter les views des modifier et supprimer

--- projetdjango/lib/views.py
+++ projetdjango/lib/views.py
@@ -20,3 +20,25 @@ def recherche(request):
 def liste_livres(request):
     livres = Livre.objects.all().select_related('Id_lib')
     return render(request, 'liste_livres.html', {'livres': livres})
+
+@login_required(login_url='Signin')
+def supprimer_livre(request, id_livre):
+    livre = get_object_or_404(Livre, Id_livre=id_livre, Id_lib=request.user)
+    if request.method == 'POST':
+        livre.delete()
+        messages.success(request, "Le livre a été supprimé avec succès.")
+        return redirect('home')
+    return render(request, 'confirmer_suppression.html', {'livre': livre})
+
+@login_required(login_url='Signin')
+def modifier_livre(request, id_livre):
+    livre = get_object_or_404(Livre, Id_livre=id_livre, Id_lib=request.user)
+    if request.method == 'POST':
+        form = LivreForm(request.POST, request.FILES, instance=livre)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Le livre a été modifié avec succès.")
+            return redirect('home')
+    else:
+        form = LivreForm(instance=livre)
+    return render(request, 'modifier_livre.html', {'form': form, 'livre': livre})
